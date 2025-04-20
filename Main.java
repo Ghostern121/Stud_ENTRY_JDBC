d3b290122dddf7438decb23c03a2062b055c045d
Ghostern121 <yashusatna2004@gmail.com>
2025-04-20 19:47:32 +0530

    added main

---
+++ Main.java
@@ -0,0 +1,9 @@
+// Main.java
+
+
+public class Main {
+    public static void main(String[] args) {
+        InputHandler inputHandler = new InputHandler();
+        inputHandler.startMenu();
+    }
+}
